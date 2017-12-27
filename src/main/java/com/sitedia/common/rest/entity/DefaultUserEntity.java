95a49710e3aebfa0a136606b2482cc1317387bd8
cedric <cedric@pavilion>
2017-12-27 23:31:43 +0100

    Fix schema usage with H2

--- src/main/java/com/sitedia/common/rest/entity/DefaultUserEntity.java
+++ src/main/java/com/sitedia/common/rest/entity/DefaultUserEntity.java
@@ -1,5 +1,41 @@
 package com.sitedia.common.rest.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.Table;
+
+import com.sitedia.common.rest.dto.RoleType;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(of = { "id" })
+@Entity
+@Table(name = "users")
+@NamedNativeQuery(name = "user.getByMail", query = "SELECT * FROM users WHERE mail = :mail", resultClass = DefaultUserEntity.class)
 public class DefaultUserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String mail;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
 
 }
--- src/main/java/com/sitedia/common/rest/service/DefaultUserService.java
+++ src/main/java/com/sitedia/common/rest/service/DefaultUserService.java
@@ -1,10 +1,16 @@
 package com.sitedia.common.rest.service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
-import com.sitedia.common.rest.dao.DaoManager;
 import com.sitedia.common.rest.entity.DefaultUserEntity;
 import com.sitedia.common.rest.exception.BusinessException;
 import com.sitedia.common.rest.exception.TechnicalException;
@@ -21,7 +27,7 @@ import com.sitedia.common.rest.mapper.DefaultUserMapper;
 public class DefaultUserService implements IUserService {
 
     @Autowired
-    private DaoManager daoManager;
+    private EntityManager entityManager;
 
     @Autowired
     private DefaultUserMapper mapper;
@@ -36,8 +42,14 @@ public class DefaultUserService implements IUserService {
      */
     @Override
     public Object getByUsername(String username) throws BusinessException, TechnicalException {
-	DefaultUserEntity result = daoManager.get(DefaultUserEntity.class, username);
-	return mapper.toDTO(result);
+	Query query = entityManager.createNamedQuery("user.getByMail");
+	query.setParameter("mail", username);
+	try {
+	    return mapper.toDTO((DefaultUserEntity) query.getSingleResult());
+	} catch (NoResultException e) {
+	    Logger.getLogger(getClass().getName()).log(Level.FINE, e.getMessage(), e);
+	    return null;
+	}
     }
 
 }
