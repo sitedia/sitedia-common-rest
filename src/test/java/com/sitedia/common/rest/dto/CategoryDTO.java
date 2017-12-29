95bdb94c9b7cf171ba8d049ceddb373ecd7f61bd
cedric <cedric@pavilion>
2017-12-29 21:35:30 +0100

    Secure category name in test

--- src/test/java/com/sitedia/common/rest/dto/CategoryDTO.java
+++ src/test/java/com/sitedia/common/rest/dto/CategoryDTO.java
@@ -1,5 +1,8 @@
 package com.sitedia.common.rest.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.sitedia.common.rest.utils.SecureTextDeserializer;
+
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,6 +25,7 @@ public class CategoryDTO {
     @ApiModelProperty(notes = "Identifier")
     private Long id;
 
+    @JsonDeserialize(using = SecureTextDeserializer.class)
     @ApiModelProperty(notes = "Name")
     private String name;
 
